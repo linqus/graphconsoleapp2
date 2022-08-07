22f47e05587154ae3a9fd4b49985ccbe3fe54b95
Karol Linkiewicz <karol@linkiewicz.pl>
2022-08-07 19:33:42 +0200

    switch to graph SDK throttling implementation

--- Program.cs
+++ Program.cs
@@ -53,52 +53,17 @@ namespace graphconsoleapp
                                                     .Build();
             return MsalAuthenticationProvider.GetInstance(cca, scopes.ToArray());
         }       
-        private static HttpClient GetAuthenticatedHTTPClient(IConfigurationRoot config)
+        private static GraphServiceClient GetAuthenticatedGraphClient(IConfigurationRoot config)
         {
             var authenticationProvider = CreateAuthorizationProvider(config);
-            var httpClient = new HttpClient(new AuthHandler(authenticationProvider, new HttpClientHandler()));
-            return httpClient;
+            var graphClient = new GraphServiceClient(authenticationProvider);
+            return graphClient;
         }
 
-        private static Message? GetMessageDetail(HttpClient client, string messageId, int defaultDelay = 2)
+        private static Microsoft.Graph.Message GetMessageDetail(GraphServiceClient client, string messageId)
         {
-        Message? messageDetail = null;
-
-        string endpoint = "https://graph.microsoft.com/v1.0/me/messages/" + messageId;
-        // submit request to Microsoft Graph & wait to process response
-        var clientResponse = client.GetAsync(endpoint).Result;
-        var httpResponseTask = clientResponse.Content.ReadAsStringAsync();
-        httpResponseTask.Wait();
-        Console.WriteLine("...Response status code: {0}  ", clientResponse.StatusCode);
-
-        // IF request successful (not throttled), set message to retrieved message
-        if (clientResponse.StatusCode == HttpStatusCode.OK)
-        {
-            messageDetail = JsonSerializer.Deserialize<Message>(httpResponseTask.Result);
-        }
-        // ELSE IF request was throttled (429, aka: TooManyRequests)...
-        else if (clientResponse.StatusCode == HttpStatusCode.TooManyRequests)
-        {
-            // get retry-after if provided; if not provided default to 2s
-            var retryAfterDelay = defaultDelay;
-            var retryAfter = clientResponse.Headers.RetryAfter;
-            if (retryAfter != null && retryAfter.Delta.HasValue && (retryAfter.Delta.Value.Seconds > 0))
-            {
-                retryAfterDelay = retryAfter.Delta.Value.Seconds;
-            }
-
-            // wait for specified time as instructed by Microsoft Graph's Retry-After header,
-            //    or fall back to default
-            Console.WriteLine(">>>>>>>>>>>>> sleeping for {0} seconds...", retryAfterDelay);
-            System.Threading.Thread.Sleep(retryAfterDelay * 1000);
-
-            // call method again after waiting
-            messageDetail = GetMessageDetail(client, messageId);
-        }
-
-        // add code here
-
-        return messageDetail;
+            // submit request to Microsoft Graph & wait to process response
+            return client.Me.Messages[messageId].Request().GetAsync().Result;
         }
 
 
@@ -110,22 +75,20 @@ namespace graphconsoleapp
                 Console.WriteLine("Invalid appsettings.json file.");
                 return;
             }
-            var client = GetAuthenticatedHTTPClient(config);
-            var profileResponse = client.GetAsync("https://graph.microsoft.com/v1.0/me").Result;
-            var profileJson = profileResponse.Content.ReadAsStringAsync().Result;
-            var profileObject = JsonDocument.Parse(profileJson);
-            var displayName = profileObject.RootElement.GetProperty("displayName").GetString();
-            Console.WriteLine("Hello " + displayName);
+            var client = GetAuthenticatedGraphClient(config);
+            var profileResponse = client.Me.Request().GetAsync().Result;
+            Console.WriteLine("Hello " + profileResponse.DisplayName);
 
             var stopwatch = new System.Diagnostics.Stopwatch();
             stopwatch.Start();
 
-            var clientResponse = client.GetAsync("https://graph.microsoft.com/v1.0/me/messages?$select=id&$top=100").Result;
-            // enumerate through the list of messages
-            var httpResponseTask = clientResponse.Content.ReadAsStringAsync();
-            httpResponseTask.Wait();
-            var graphMessages = JsonSerializer.Deserialize<Messages>(httpResponseTask.Result);
-            var items = graphMessages == null ? Array.Empty<Message>() : graphMessages.Items;
+            var clientResponse = client.Me.Messages
+                                        .Request()
+                                        .Select(m => new { m.Id })
+                                        .Top(100)
+                                        .GetAsync()
+                                        .Result;
+            var items = clientResponse.CurrentPage;
 
             var tasks = new List<Task>();
             foreach (var graphMessage in items)
