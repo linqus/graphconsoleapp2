5f405f71eb426d7ca087be0a582a437251941b8d
Karol Linkiewicz <karol@linkiewicz.pl>
2022-08-07 18:57:55 +0200

    add messages helper for strong typing

---
+++ Messages.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Text.Json.Serialization;
+
+namespace graphconsoleapp
+{
+  public class Messages {
+    [JsonPropertyName("@odata.context")]
+    public string ODataContext {get; set;} = string.Empty;
+    [JsonPropertyName("@odata.nextLink")]
+    public string ODataNextLink {get; set;} = string.Empty;
+
+    [JsonPropertyName("value")]
+    public Message[] Items {get; set;} = Array.Empty<Message>();
+  }
+
+  public class Message {
+    [JsonPropertyName("@odata.etag")]
+    public string ETag {get; set;} = string.Empty;
+
+    [JsonPropertyName("id")]
+    public string Id {get; set;} = string.Empty;
+
+    [JsonPropertyName("subject")]
+    public string Subject {get; set;} = string.Empty;
+  }
+}
