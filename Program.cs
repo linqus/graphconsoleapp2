93b7fe46c18ec36fa38f97533ce939d178ecfa52
Karol Linkiewicz <karol@linkiewicz.pl>
2022-08-07 17:24:33 +0200

    add main program

--- Program.cs
+++ Program.cs
@@ -15,9 +15,106 @@ namespace graphconsoleapp
 {
     public class Program
     {
+
+        private static IConfigurationRoot? LoadAppSettings()
+        {
+            try
+            {
+                var config = new ConfigurationBuilder()
+                                .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json", false, true)
+                                .Build();
+
+                if (string.IsNullOrEmpty(config["applicationId"]) ||
+                    string.IsNullOrEmpty(config["tenantId"]))
+                {
+                return null;
+                }
+
+                return config;
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                return null;
+            }
+        }
+
+        private static IAuthenticationProvider CreateAuthorizationProvider(IConfigurationRoot config)
+        {
+            var clientId = config["applicationId"];
+            var authority = $"https://login.microsoftonline.com/{config["tenantId"]}/v2.0";
+
+            List<string> scopes = new List<string>();
+            scopes.Add("https://graph.microsoft.com/.default");
+
+            var cca = PublicClientApplicationBuilder.Create(clientId)
+                                                    .WithAuthority(authority)
+                                                    .WithDefaultRedirectUri()
+                                                    .Build();
+            return MsalAuthenticationProvider.GetInstance(cca, scopes.ToArray());
+        }       
+        private static HttpClient GetAuthenticatedHTTPClient(IConfigurationRoot config)
+        {
+            var authenticationProvider = CreateAuthorizationProvider(config);
+            var httpClient = new HttpClient(new AuthHandler(authenticationProvider, new HttpClientHandler()));
+            return httpClient;
+        }
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = LoadAppSettings();
+            if (config == null)
+            {
+                Console.WriteLine("Invalid appsettings.json file.");
+                return;
+            }
+            var client = GetAuthenticatedHTTPClient(config);
+            var profileResponse = client.GetAsync("https://graph.microsoft.com/v1.0/me").Result;
+            var profileJson = profileResponse.Content.ReadAsStringAsync().Result;
+            var profileObject = JsonDocument.Parse(profileJson);
+            var displayName = profileObject.RootElement.GetProperty("displayName").GetString();
+            Console.WriteLine("Hello " + displayName);
+
+            var totalRequests = 100;
+            var successRequests = 0;
+            var tasks = new List<Task>();
+            var failResponseCode = HttpStatusCode.OK;
+            HttpResponseHeaders failedHeaders = null!;
+
+            for (int i = 0; i < totalRequests; i++)
+            {
+            tasks.Add(Task.Run(() =>
+            {
+                var response = client.GetAsync("https://graph.microsoft.com/v1.0/me/messages").Result;
+                Console.Write(".");
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                successRequests++;
+                }
+                else
+                {
+                Console.Write('X');
+                failResponseCode = response.StatusCode;
+                failedHeaders = response.Headers;
+                }
+            }));
+            }
+
+            var allWork = Task.WhenAll(tasks);
+            try
+            {
+            allWork.Wait();
+            }
+            catch { }
+            Console.WriteLine();
+            Console.WriteLine("{0}/{1} requests succeeded.", successRequests, totalRequests);
+            if (successRequests != totalRequests)
+            {
+            Console.WriteLine("Failed response code: {0}", failResponseCode.ToString());
+            Console.WriteLine("Failed response headers: {0}", failedHeaders);
+            }
+
+
+
         }
     }
 }
